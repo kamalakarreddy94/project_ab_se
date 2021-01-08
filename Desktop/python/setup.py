dd20506a7384108637c7b57a19329e11454c29be
kamalakarreddy94 <30488299+kamalakarreddy94@users.noreply.github.com>
2021-01-07 21:07:38 -0500

    Update setup.py

--- Desktop/python/setup.py
+++ Desktop/python/setup.py
@@ -3,7 +3,7 @@ setup(name = "project",
     version = "0.0.1",
     description = "project"
     packages = "project",
-    install_requires= ["pandas==1.0.1"]
+    install_requires= ["pandas==1.0.1"],
     zip_safe = True
 
 )
